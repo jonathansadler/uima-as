2b849d4ec0e1af620af100e0ef1589bcc7127fc1
Marshall Schor <schor@apache.org>
2009-11-03 17:07:36 +0000

    UIMA-1647 make work with dash shell used by Ubuntu, by removing . invocations and adding exports of vars intended to be seen by runUimaClass script
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uima-as/trunk@832476 13f79535-47bb-0310-9956-ffa450edef68

--- uima-as-distr/src/main/scripts/dd2spring.sh
+++ uima-as-distr/src/main/scripts/dd2spring.sh
@@ -29,5 +29,5 @@ then
 else
   UIMA_JAVA_CALL="$JAVA_HOME/bin/java"
 fi
-. "$UIMA_HOME/bin/setUimaClassPath.sh"
+"$UIMA_HOME/bin/setUimaClassPath.sh"
 "$UIMA_JAVA_CALL" -cp "$UIMA_CLASSPATH:$UIMA_HOME/saxon/saxon8.jar" "-Duima.datapath=$UIMA_DATAPATH" -Xmx256M net.sf.saxon.Transform -l -s $1 -o $2 $UIMA_HOME/bin/dd2spring.xsl $3
--- uima-as-distr/src/main/scripts/runRemoteAsyncAE.sh
+++ uima-as-distr/src/main/scripts/runRemoteAsyncAE.sh
@@ -23,4 +23,4 @@ then
   exit 1
 fi
 
-. "$UIMA_HOME/bin/runUimaClass.sh" org.apache.uima.examples.as.RunRemoteAsyncAE $*
+"$UIMA_HOME/bin/runUimaClass.sh" org.apache.uima.examples.as.RunRemoteAsyncAE $*
