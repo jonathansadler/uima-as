c4f9b2980ad7ddf9a3061acbb90dd1226565acb9
Jaroslaw Cwiklik <cwiklik@apache.org>
2010-04-27 19:55:16 +0000

    UIMA-1772 Fixed Aggregate log entry while executing a flow step
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-as/trunk@938625 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-as-core/src/main/java/org/apache/uima/aae/controller/AggregateAnalysisEngineController_impl.java
+++ uimaj-as-core/src/main/java/org/apache/uima/aae/controller/AggregateAnalysisEngineController_impl.java
@@ -1242,7 +1242,7 @@ public class AggregateAnalysisEngineController_impl extends BaseAnalysisEngineCo
         if (UIMAFramework.getLogger(CLASS_NAME).isLoggable(Level.FINEST)) {
           UIMAFramework.getLogger(CLASS_NAME).logrb(Level.FINEST, CLASS_NAME.getName(),
                   "simpleStep", UIMAEE_Constants.JMS_LOG_RESOURCE_BUNDLE,
-                  "UIMAEE_next_step__FINEST", new Object[] {analysisEngineKey, aCasReferenceId  });
+                  "UIMAEE_next_step__FINEST", new Object[] { aCasReferenceId, analysisEngineKey  });
         }
 
         // Reset number of parallel delegates back to one. This is done only if the previous step
