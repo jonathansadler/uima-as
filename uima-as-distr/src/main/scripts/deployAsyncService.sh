d18e230f8cc53905011db06bcda134733e607c47
Marshall Schor <schor@apache.org>
2010-10-21 14:41:55 +0000

    [UIMA-1883] recommitting this to trunk, was accidentally commited to branch
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-as/trunk@1026014 13f79535-47bb-0310-9956-ffa450edef68

--- uima-as-distr/src/main/scripts/deployAsyncService.sh
+++ uima-as-distr/src/main/scripts/deployAsyncService.sh
@@ -31,4 +31,4 @@ if [ ! -f $1 ]
        exit 1;
 fi;
 
-"$UIMA_HOME/bin/runUimaClass.sh" org.apache.uima.adapter.jms.service.UIMA_Service -saxonURL "file:$UIMA_HOME/saxon/saxon8.jar" -xslt "$UIMA_HOME/bin/dd2spring.xsl" -dd $*
+"$UIMA_HOME/bin/runUimaClass.sh" org.apache.uima.adapter.jms.service.UIMA_Service -saxonURL "file:$UIMA_HOME/saxon/saxon8.jar" -xslt "$UIMA_HOME/bin/dd2spring.xsl" -dd $@
