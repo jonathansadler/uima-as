eb0982afd6848448489e1760adfb7acccf2c642e
Jaroslaw Cwiklik <cwiklik@apache.org>
2011-01-27 22:08:47 +0000

    UIMA-1876 Replaced references to activemq 4.1.1 with 5.4.1
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-as/trunk@1064333 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-as-activemq/src/main/java/org/apache/uima/aae/jmx/monitor/JmxMonitor.java
+++ uimaj-as-activemq/src/main/java/org/apache/uima/aae/jmx/monitor/JmxMonitor.java
@@ -625,7 +625,7 @@ public class JmxMonitor implements Runnable {
 
   private static void printUsage() {
     System.out
-            .println("Usage: \njava -cp %UIMA_HOME%/lib/uimaj-as-activemq.jar;%UIMA_HOME%/lib/uimaj-as-core.jar;%UIMA_HOME%/lib/uima-core.jar;%UIMA_HOME%/apache-activemq-4.1.1/apache-activemq-4.1.1.jar -Djava.util.logging.config.file=%UIMA_HOME%/config/MonitorLogger.properties    org.apache.uima.aae.jmx.monitor.JmxMonitor uri <frequency>\nuri - jmx server URI- required\nfrequency - how often the checkpoint is done. Default: 1000ms - optional");
+            .println("Usage: \njava -cp %UIMA_HOME%/lib/*;%UIMA_HOME%/apache-activemq-5.4.1/* -Djava.util.logging.config.file=%UIMA_HOME%/config/MonitorLogger.properties    org.apache.uima.aae.jmx.monitor.JmxMonitor uri <frequency>\nuri - jmx server URI- required\nfrequency - how often the checkpoint is done. Default: 1000ms - optional");
   }
 
   private static void echo(String msg) {
