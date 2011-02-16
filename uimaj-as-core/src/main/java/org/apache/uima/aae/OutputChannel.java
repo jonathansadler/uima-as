508425f45c2f3a102ec2a247edbce98e0ab6b442
Jaroslaw Cwiklik <cwiklik@apache.org>
2011-02-16 18:04:17 +0000

    UIMA-2055 Removed methods no longer needed after refactoring JmsOutputChannel.java
    git-svn-id: https://svn.apache.org/repos/asf/uima/uima-as/trunk@1071335 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-as-core/src/main/java/org/apache/uima/aae/OutputChannel.java
+++ uimaj-as-core/src/main/java/org/apache/uima/aae/OutputChannel.java
@@ -23,8 +23,6 @@ import org.apache.uima.aae.InProcessCache.CacheEntry;
 import org.apache.uima.aae.controller.AnalysisEngineController;
 import org.apache.uima.aae.controller.Endpoint;
 import org.apache.uima.aae.error.AsynchAEException;
-import org.apache.uima.analysis_engine.metadata.AnalysisEngineMetaData;
-import org.apache.uima.cas.CAS;
 import org.apache.uima.resource.metadata.ProcessingResourceMetaData;
 
 public interface OutputChannel extends Channel {
@@ -32,32 +30,14 @@ public interface OutputChannel extends Channel {
 
   public void initialize() throws AsynchAEException;
 
-  public void sendRequest(String aCasReferenceId, Endpoint anEndpoint) throws AsynchAEException;
-
-  public void sendRequest(String aCasReferenceId, Endpoint[] anEndpoint) throws AsynchAEException;
-
-  public void sendRequest(int aCommand, Endpoint anEndpoint) throws AsynchAEException;
-
   public void sendRequest(int aCommand, String aCasReferenceId, Endpoint anEndpoint)
-          throws AsynchAEException;
-
-  public void sendReply(int aCommand, Endpoint anEndpoint) throws AsynchAEException;
-
-  public void sendReply(int aCommand, Endpoint anEndpoint, String aCasReferenceId)
           throws AsynchAEException;
 
   public void sendReply(int aCommand, Endpoint anEndpoint, String aCasReferenceId, boolean notifyOnJmsException)
   throws AsynchAEException;
 
-  public void sendReply(CAS aCas, String anInputCasReferenceId, String aNewCasReferenceId,
-          Endpoint anEndpoint, long sequence) throws AsynchAEException;
-
-  public void sendReply(String aCasReferenceId, Endpoint anEndpoint) throws AsynchAEException;
-
   public void sendReply(CacheEntry entry, Endpoint anEndpoint) throws AsynchAEException;
 
-  // public void sendReply( AnalysisEngineMetaData anAnalysisEngineMetadata, Endpoint anEndpoint,
-  // boolean serialize ) throws AsynchAEException;
   public void sendReply(ProcessingResourceMetaData aProcessingResourceMetadata,
           Endpoint anEndpoint, boolean serialize) throws AsynchAEException;
 
